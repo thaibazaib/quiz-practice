16d0ee8180714ca84cb245e0e8e3d93b44679956
thaibazaib <thaibazaib@gmail.com>
2022-02-12 10:14:29 +0000

    update game.js with questions

---
+++ game.js
@@ -0,0 +1,105 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const score = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let available questions = []
+
+let questions = [
+    {
+     question: 'What is 2 + 2?',
+     choice1: '2',
+     choice2: '4',
+     choice3: '21',
+     choice4: '17',
+     answer: 2,
+    },
+    {
+    question: 'The talest building in the world is located in which city?',
+    choice1: 'Dubai',
+    choice2: 'New York',
+    choice3: 'Shanghai',
+    choice4: 'None of the above',
+    answer: 1,
+    },
+    {
+    question: 'What percent of American adults believe that choclate milk comes from brown cows?',
+    choice1: '20%',
+    choice2: '18%',
+    choice3: '7%',
+    choice4: '33%',
+    answer: 3,
+    },
+    {
+    question: 'Approximately what percent of U.S. power outages are caused by suirrels?',
+    choice1: '10-20%',
+    choice2: '5-10%',
+    choice3: '15-20%',
+    choice4: '30-40%',
+    answer: 1,
+    }
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+    
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+    const questionIndex = Math.floor(Math.random( * availableQuestions.length)
+    current Question = availableQuestions[questionIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestions['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choices => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        cons selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct'){
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
